69fbebe0daa31c397391158fa3b5ad12f4008fc7
Andrew Silin <andrejss@scandi.com>
2016-04-26 21:02:47 +0300

    Added category output to home page.

--- src/main/java/lv/javaguru/shop/controller/HomeController.java
+++ src/main/java/lv/javaguru/shop/controller/HomeController.java
@@ -1,7 +1,9 @@
 package lv.javaguru.shop.controller;
 
 import lv.javaguru.shop.entity.Product;
+import lv.javaguru.shop.entity.Category;
 import lv.javaguru.shop.repository.ProductRepository;
+import lv.javaguru.shop.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
+
         Iterable<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
+
+        Iterable<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
 
         return "index";
     }
